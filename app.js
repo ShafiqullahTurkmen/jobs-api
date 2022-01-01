27270f4e7b43a1a9a4bb0a732ad5663f12332b25
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-01-01 15:54:36 +0300

    custom error handler and security packages added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
+const helmet = require("helmet")
+const cors = require("cors")
+const rateLimit = require("express-rate-limit")
+const xss = require("xss-clean")
 const connectDB = require("./db/connect");
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -10,8 +14,17 @@ const authenticateUser = require("./middleware/authentication");
 
 const app = express();
 
+app.set('trust proxy', 1);
+app.use(rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}));
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+
 
 // routes
 app.use("/api/v1/auth", authRouter);
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,16 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  let customError = {
+    //set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong try again later",
+  };
 
-module.exports = errorHandlerMiddleware
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
